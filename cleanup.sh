e020aabc3a24edd5f11b3e673c5a3af56b8ae22d
Wojtek Kosierkiewicz <wojtekkosierkiewicz@gmail.com>
2022-03-02 17:07:21 +0100

    cleanups

--- cleanup.sh
+++ cleanup.sh
@@ -1,10 +1,14 @@
-#remove nix
-rm -rf $HOME/{.nix-channels,.nix-defexpr,.nix-profile,.config/nixpkgs}
-sudo rm -rf /nix
-
+#remove stowed config
 stow -D git
 stow -D zsh
 stow -D nvim
+
+#change shell back to bash
+sudo chsh -s $(which bash) $USER
+
+#remove nix
+rm -rf $HOME/{.nix-channels,.nix-defexpr,.nix-profile,.config/nixpkgs}
+sudo rm -rf /nix
 
 #remove .dotfiles
 rm -rf $HOME/.dotfiles
